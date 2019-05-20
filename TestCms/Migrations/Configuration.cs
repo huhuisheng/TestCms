8ef4eeba62dfb9f3837ccf9d80d7ffa4c0f1bbf6
huhuisheng <1469953509@qq.com>
2019-05-20 20:59:57 +0800

    添加

---
+++ TestCms/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace TestCms.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<TestCms.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(TestCms.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
